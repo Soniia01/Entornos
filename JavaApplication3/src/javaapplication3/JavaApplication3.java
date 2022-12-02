f5d167a4003921643a9958e57d65fa032913991d
Sonia <alumno@DESKTOP-TDJHAIG>
2022-12-02 12:07:22 +0100

    CommitedBitch

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -14,6 +14,8 @@ public class JavaApplication3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("hello");
         // TODO code application logic here
     }
     
